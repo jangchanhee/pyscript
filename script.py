2f1667b2b68483607e266eb36a44269865cc659a
jangchanhee <109408195+jangchanhee@users.noreply.github.com>
2022-07-16 16:25:54 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://github.com/jangchanhee" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -21,19 +21,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["green", "blue"]
+colors = ["#CCCCCC", "blue"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "장찬희")
+write("description", "효성중3학년")
+write("button", "github")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "좋아하는 것": "폰",
+  "싫어하는 것": "공포 관련",
+  "인스타 아이디": "jang._.07",
+  "요즘 보는 드라마": "이상한 변호사 우영우"
 }
 information(informations)
